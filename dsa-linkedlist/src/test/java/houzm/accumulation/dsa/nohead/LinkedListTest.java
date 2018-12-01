5d5451847c2a36545e762c42dd703f5f0b63a7a4
hzm-January <hzm_dream@163.com>
2018-12-01 22:25:14 +0800

    linked list test

---
+++ dsa-linkedlist/src/test/java/houzm/accumulation/dsa/nohead/LinkedListTest.java
@@ -0,0 +1,231 @@
+package houzm.accumulation.dsa.nohead;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
+
+/**
+ * LinkedList Tester.
+ *
+ * @author houzm
+ * @version 1.0
+ * @since <pre>12/01/2018</pre>
+ */
+public class LinkedListTest {
+    LinkedList<Integer> list = new LinkedList<>();
+    LinkedList<Integer> listPalindrome = new LinkedList<>();
+
+    @Before
+    public void before() throws Exception {
+        list.addToLast(1);
+        list.addToLast(2);
+        list.addToLast(3);
+        list.addToLast(4);
+        list.addToLast(5);
+        list.addToLast(6);
+        listPalindrome.addToLast(1);
+        listPalindrome.addToLast(2);
+        listPalindrome.addToLast(3);
+        listPalindrome.addToLast(3);
+        listPalindrome.addToLast(2);
+        listPalindrome.addToLast(1);
+    }
+
+    @After
+    public void after() throws Exception {
+    }
+
+    /**
+     * Method: addToLast(T t)
+     */
+    @Test
+    public void testAddToLast() throws Exception {
+//TODO: Test goes here...
+    }
+
+    /**
+     * Method: addToHead(T t)
+     */
+    @Test
+    public void testAddToHead() throws Exception {
+//TODO: Test goes here...
+    }
+
+    /**
+     * Method: addAfter(Node node, T t)
+     */
+    @Test
+    public void testAddAfter() throws Exception {
+//TODO: Test goes here...
+    }
+
+    /**
+     * Method: addBefore(Node node, T t)
+     */
+    @Test
+    public void testAddBefore() throws Exception {
+//TODO: Test goes here...
+    }
+
+    /**
+     * Method: deleteLast()
+     */
+    @Test
+    public void testDeleteLast() throws Exception {
+//TODO: Test goes here...
+    }
+
+    /**
+     * Method: deleteFirst()
+     */
+    @Test
+    public void testDeleteFirst() throws Exception {
+//TODO: Test goes here...
+    }
+
+    /**
+     * Method: deleteByNode(Node node)
+     */
+    @Test
+    public void testDeleteByNode() throws Exception {
+//TODO: Test goes here...
+    }
+
+    /**
+     * Method: deleteByValue(T t)
+     */
+    @Test
+    public void testDeleteByValue() throws Exception {
+        list.printAll();
+        list.deleteByValue(3);
+        list.printAll();
+    }
+
+    /**
+     * Method: palindrome()
+     */
+    @Test
+    public void testPalindrome() throws Exception {
+        Assert.assertEquals(true, listPalindrome.palindrome());
+        Assert.assertEquals(true, list.palindrome());
+    }
+
+    /**
+     * Method: inverse(Node p)
+     */
+    @Test
+    public void testInverseP() throws Exception {
+//TODO: Test goes here...
+    }
+
+    /**
+     * Method: inverse()
+     */
+    @Test
+    public void testInverse() throws Exception {
+        LinkedList.Node head = list.inverse();
+        while (head != null) {
+            System.out.print(head.getData() + " ");
+            head = head.getNext();
+        }
+        System.out.println();
+    }
+
+    /**
+     * Method: printAll()
+     */
+    @Test
+    public void testPrintAll() throws Exception {
+        list.printAll();
+    }
+
+    /**
+     * Method: getData()
+     */
+    @Test
+    public void testGetData() throws Exception {
+//TODO: Test goes here...
+    }
+
+    /**
+     * Method: setData(T data)
+     */
+    @Test
+    public void testSetData() throws Exception {
+//TODO: Test goes here...
+    }
+
+    /**
+     * Method: getNext()
+     */
+    @Test
+    public void testGetNext() throws Exception {
+//TODO: Test goes here...
+    }
+
+    /**
+     * Method: setNext(Node next)
+     */
+    @Test
+    public void testSetNext() throws Exception {
+//TODO: Test goes here...
+    }
+
+
+    /**
+     * Method: deleteOnlyHead()
+     */
+    @Test
+    public void testDeleteOnlyHead() throws Exception {
+//TODO: Test goes here...
+/*
+try {
+   Method method = LinkedList.getClass().getMethod("deleteOnlyHead");
+   method.setAccessible(true);
+   method.invoke(<Object>, <Parameters>);
+} catch(NoSuchMethodException e) {
+} catch(IllegalAccessException e) {
+} catch(InvocationTargetException e) {
+}
+*/
+    }
+
+
+    /**
+     * Method: linkLast(T t)
+     */
+    @Test
+    public void testLinkLast() throws Exception {
+//TODO: Test goes here...
+/*
+try {
+   Method method = LinkedList.getClass().getMethod("linkLast", T.class);
+   method.setAccessible(true);
+   method.invoke(<Object>, <Parameters>);
+} catch(NoSuchMethodException e) {
+} catch(IllegalAccessException e) {
+} catch(InvocationTargetException e) {
+}
+*/
+    }
+
+    /**
+     * Method: compareLF(Node left, Node right)
+     */
+    @Test
+    public void testCompareLF() throws Exception {
+//TODO: Test goes here...
+/*
+try {
+   Method method = LinkedList.getClass().getMethod("compareLF", Node.class, Node.class);
+   method.setAccessible(true);
+   method.invoke(<Object>, <Parameters>);
+} catch(NoSuchMethodException e) {
+} catch(IllegalAccessException e) {
+} catch(InvocationTargetException e) {
+}
+*/
+    }
+
+} 

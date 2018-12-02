da7aac36fe233a4bbdf6bfebb48eee89bd59a209
hzm-January <hzm_dream@163.com>
2018-12-02 18:16:04 +0800

    顺序栈--扩容缩容

---
+++ dsa-stack/src/main/java/houzm/accumulation/dsa/SequenceStackDynamicAllocationDeep.java
@@ -0,0 +1,112 @@
+package houzm.accumulation.dsa;
+
+/**
+ * Author: hzm_dream@163.com
+ * Date:  2018/12/1 23:10
+ * Modified By:
+ * Description：顺序栈--支持扩容
+ * <p>
+ * 栈的操作：压栈，弹栈(出栈)
+ */
+public class SequenceStackDynamicAllocationDeep<T> {
+    //栈
+    private T[] stack;
+    // 栈的深度
+    private Integer deep;
+    // 栈中元素个数
+    private int size;
+    // 默认栈的深度
+    private static final Integer DEFUALT_LENGTH = 16;
+    // 最深栈深度
+    private static final Integer MAX_STACK_DEEP = Integer.MAX_VALUE-8;
+
+    public SequenceStackDynamicAllocationDeep() {
+        this.deep = DEFUALT_LENGTH;
+        this.size = 0; //方便理解，可省略
+        stack = (T[]) new Object[deep];
+    }
+
+    public SequenceStackDynamicAllocationDeep(int initialCapacity) {
+        this.deep = initialCapacity;
+        this.size = 0; //方便理解，可省略
+        stack = (T[]) new Object[deep];
+    }
+
+    /**
+     * 压栈
+     *
+     * @param t
+     * @return
+     */
+    public boolean push(T t) {
+        //1. 栈满校验
+        if (size == deep) {
+            resizeGrow(deep + deep >> 1);
+        }
+        //2. 压栈
+        stack[size] = t;
+        ++size;
+        return true;
+    }
+
+    /**
+     * 出栈
+     *
+     * @return
+     */
+    public T pop() {
+        //1. 栈空校验
+        if (size == 0) {
+            throw new IllegalStateException(" stack is empty ");
+        }
+        //2. 取出value
+        T popValue = stack[size - 1];
+        --size;
+        resizeReduce(deep>>1); //0.5
+        return popValue;
+    }
+
+    /**
+     * 扩容
+     */
+    private void resizeGrow(int minCapacity) {
+        if (minCapacity < 0) {
+            throw new IllegalStateException(" capacity illegal ");
+        }
+        int oldCapacity = stack.length;
+        int newCapacity = oldCapacity + oldCapacity >> 1; //1.5
+        if (newCapacity - minCapacity < 0) {
+            newCapacity = minCapacity;
+        }
+        if (newCapacity - MAX_STACK_DEEP > 0) {
+            if (newCapacity > MAX_STACK_DEEP) {
+                newCapacity = Integer.MAX_VALUE;
+            } else {
+                newCapacity = MAX_STACK_DEEP;
+            }
+        }
+        T[] newStack = (T[]) new Object[newCapacity];
+        System.arraycopy(stack, 0, newStack, 0, stack.length);
+        stack = newStack;
+    }
+    /**
+     * 缩容
+     * @param minCapacity
+     */
+    private void resizeReduce(int minCapacity) {
+        if (minCapacity < 0) {
+            throw new IllegalStateException(" capacity illegal ");
+        }
+        if (size == stack.length / 4 && stack.length / 2 != 0) {
+            int newCapacity = stack.length >> 1; //0.5
+            if (minCapacity < newCapacity) {
+                newCapacity = minCapacity;
+            }
+            T[] newStack = (T[]) new Object[newCapacity];
+            System.arraycopy(stack, 0, newStack, 0, newCapacity);
+            stack = newStack;
+        }
+    }
+
+
+}

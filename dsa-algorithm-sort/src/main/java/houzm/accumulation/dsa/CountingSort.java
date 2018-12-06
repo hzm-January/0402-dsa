056e51b00eafa9b70550adf129986ccc23cb54bf
hzm-January <hzm_dream@163.com>
2018-12-06 15:00:03 +0800

    计数排序

--- dsa-algorithm-sort/src/main/java/houzm/accumulation/dsa/CountingSort.java
+++ dsa-algorithm-sort/src/main/java/houzm/accumulation/dsa/CountingSort.java
@@ -6,11 +6,19 @@ import java.util.Arrays;
  * author: hzm_dream@163.com
  * date: 2018/12/6 11:58
  * description: 计数排序
- * <p>
+ *
  * 描述：使用桶排序思想
  * 分析：
- * 1. 获取数组中最大值、最小值
- * 2. 获取数组中
+ * 若原始数组为arr
+ * 1. 获取数组中最大值max、最小值min
+ * 2. 桶个数bucketNum=max-min+1
+ * 3. 定义桶数组int[] countingArr = new int[bucketNum];说明：原始数组的元素个数保存在桶数组的索引值=原始数组数值-最小值
+ * 4. 统计原始数组中元素出现的次数x，countingArr[arr[i]-min] = x;
+ * 5. 统计桶数组，桶数组中记录的元素在原始数组中出现的个数累计，统计出 countingArr[k] = k+min元素在原始数组中个数+小于k+min元素在原始数组中个数
+ * 举例：[1, 0, 2, 2, 1, 0, 1]  统计之后为  [1, 1, 3, 5, 6, 6, 7]
+ * 6. 定义存放最终结果的数组int[] afterSortArr = new int[arr.length];
+ * 7. 遍历取原始数组arr中的元素arr[i]，同时取出countingArr中对应该元素的值int index = countingArr[arr[i]-min]
+ *      afterSortArr[index-1]= arr[i], 并对 countingArr[arr[i]-min] 减一
  */
 public class CountingSort {
 
@@ -43,9 +51,9 @@ public class CountingSort {
             countingArr[i] = count;
         }
         System.out.println("桶数组统计结果：" + Arrays.toString(countingArr));
-        //5. 定义排序后的最终数组
+        //6. 定义排序后的最终数组
         int[] arrAfterSort = new int[arr.length];
-        //6. 从后往前扫描原始数组，并找到在桶中的个数
+        //7. 从后往前扫描原始数组，并找到在统计后桶中的值-1，即为该元素在原始索引
         for (int i = arr.length - 1; i >= 0; i--) {
             int index = countingArr[arr[i] - min];
             arrAfterSort[index - 1] = arr[i];
